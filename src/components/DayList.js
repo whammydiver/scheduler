3f62caef0029e09735f7bcd7ed3d8fc7dfc23c21
Paul Taylor <paul@whammydiver.com>
2022-09-11 17:36:33 -0400

    DayList rendered in Application

---
+++ src/components/DayList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+  const days = props.days.map((day) => (
+    <DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      setDay={props.setDay}
+      selected={day.name === props.day}
+    />
+  ));
+
+  return <ul>{days}</ul>;
+}
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,21 +3,20 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-
-  const formatSpots = function(spots) {
+  const formatSpots = function (spots) {
     if (spots === 0) {
-      return "no spots remaining"
+      return "no spots remaining";
     } else if (spots === 1) {
-      return "1 spot remaining"
+      return "1 spot remaining";
     } else {
-      return `${spots} spots remaining`
+      return `${spots} spots remaining`;
     }
-  }
+  };
 
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full": !props.spots
-  })
+    "day-list__item--full": !props.spots,
+  });
 
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
